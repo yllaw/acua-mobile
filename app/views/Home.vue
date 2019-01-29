d77ed06164cb19f95f5b8d217fa444e52e607cbb
Placencia93 <jpthegamer016@gamil.com>
2019-01-29 12:22:30 -0800

    changing the homepage UI

--- app/views/Home.vue
+++ app/views/Home.vue
@@ -3,21 +3,21 @@
     <ActionBar :title="actionBar"/>
       <StackLayout>
 
-      <SearchBar v-model="search" hint="Enter text..." />
+      <!-- SearchBar v-model="search" hint="Enter text..." / -->
 
 
-      <Label class="message" :text="search" />
+      <!--Label class="message" :text="search" /-->
 
-      <GridLayout columns="*,*" rows="*,*,*">
-        <CategoryButton margin="10" row="0" col="0" :title="titles[0]" :action="toFaq" backgroundColor="#6AFF5A" />
+      <GridLayout class="layout" columns="*,*" rows="*,*,*">
+        <CategoryButton margin="10" row="0" col="0" :title="titles[0]" :action="toFaq" class="HomeButton1"/>
               
-        <CategoryButton margin="10" row="0" col="1" :title="titles[1]" :action="toFaq" backgroundColor="#ff0080" />
+        <CategoryButton margin="10" row="0" col="1" :title="titles[1]" :action="toFaq" class="HomeButton2"/>
               
-        <CategoryButton margin="10" row="1" col="0" :title="titles[2]" :action="toFaq" backgroundColor="#00ffbf" />
+        <CategoryButton margin="10" row="1" col="0" :title="titles[2]" :action="toFaq" class="HomeButton3"/>
               
-        <CategoryButton margin="10" row="1" col="1" :title="titles[3]" :action="toFaq" backgroundColor="#0040ff" />
+        <CategoryButton margin="10" row="1" col="1" :title="titles[3]" :action="toFaq" class="HomeButton4"/>
                 
-        <CategoryButton margin="10" row="2" col="0" :title="titles[4]" :action="toFaq" backgroundColor="#8000ff" />
+        <CategoryButton margin="10" row="2" col="0" :title="titles[4]" :action="toFaq" class="HomeButton5"/>
       </GridLayout>
 
       <Label class="message" :text="search" />
@@ -34,7 +34,7 @@ import Faq from "./Faq.vue"
 export default {
   data() {
     return {
-      actionBar: "ACUA Home",
+      actionBar: "Welcome",
       msg: "Hello World!",
       titles: [
         "Adopt an Animal!",
@@ -43,7 +43,7 @@ export default {
         "FAQ",
         "Contact Us"
       ],
-      search: "this will change as you type",
+      //search: "this will change as you type",
       results: [],
       faq: Faq
     };
@@ -64,15 +64,84 @@ export default {
 </script>
 
 <style scoped>
-ActionBar {
-  background-color: #53ba82;
-  color: #ffffff;
-}
+    ActionBar {
+        background-color: #009fca;
+        color: #ffffff;
+    }
 
-.message {
-  text-align: center;
-  font-size: 20;
-  color: #333333;
-}
+    .S_Bar{
+        color: #050505
+    }
+
+    .HomeButton1{
+        text: Adopt an Animal;
+        background:linear-gradient(180deg, rgb(255, 157, 157), rgb(255, 89, 60));
+        font-size: 35;
+        color: #930000;
+        width: 200;
+        height: 200;
+        border-radius: 25;
+        font-weight: bold;
+        letter-spacing: 0;
+    }
+
+    .HomeButton2{
+        text: FAQ;
+        background:linear-gradient(180deg, lightgreen, rgb(82, 255, 73));
+        font-size: 50;
+        color: #0b4e01;
+        width: 200;
+        height: 200;
+        border-radius: 25;
+        font-weight: bold;
+        letter-spacing: 0;
+    }
+
+    .HomeButton3{
+        text: Servies & Fees;
+        background:linear-gradient(180deg, rgb(255, 219, 151), rgb(255, 175, 55));
+        font-size: 35;
+        color: rgb(59, 38, 0);
+        width: 200;
+        height: 200;
+        border-radius: 25;
+        font-weight: bold;
+        letter-spacing: 0;
+    }
+
+    .HomeButton4{
+        text: Servies Request;
+        background:linear-gradient(180deg, rgb(255, 255, 255), rgb(179, 179, 179));
+        font-size: 30;
+        color: #323232;
+        width: 200;
+        height: 200;
+        border-radius: 25;
+        font-weight: bold;
+        letter-spacing: 0;
+    }
+
+    .HomeButton5{
+        text: Contact Us;
+        background:linear-gradient(180deg, rgb(240, 175, 255),  rgb(208, 0, 255));
+        font-size: 30;
+        color: #1f0025;
+        width: 200;
+        height: 200;
+        border-radius: 25;
+        font-weight: bold;
+        letter-spacing: 0;
+    }
+    
+    .layout{
+        background-color: #e2f6ff
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #252525;
+    }
 </style>
 
