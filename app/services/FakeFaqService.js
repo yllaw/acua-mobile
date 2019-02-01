ac2a475dfdeb2406423181955662a147006434b5
fgudino3 <frankyjg96@aol.com>
2019-01-31 16:30:14 -0800

    separated data into dedicated json from fake service

--- app/services/FakeFaqService.js
+++ app/services/FakeFaqService.js
@@ -1,20 +1,18 @@
-const faqs = [
-  {
-    question: 'what?',
-    answer: 'do this'
-  },
-  {
-    question: 'how?',
-    answer: 'like this'
-  }
-]
+import { faqs } from '@/db/faqs.json'
 
 export default {
   getFaqs () {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({ data: faqs })
-      })
-    }, 10)
+      }, 10)
+    })
+  },
+  addFaq (entry) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: entry })
+      }, 10)
+    })
   }
 }
