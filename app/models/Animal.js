c862db96753eb4cbc32ad86a149bf7c6597ecb9a
Marco Argentieri <marco.argentieri@hotmail.com>
2019-04-19 22:14:55 -0700

    init adoption service

---
+++ app/models/Animal.js
@@ -0,0 +1,26 @@
+export default class {
+    constructor({animal_id, animal_name, animal_type, sex, age, DOB, primary_color, secondary_color, breed_group, primary_breed, secondary_breed, animal_stat, stamp  }) {
+        this.animal_id = animal.animalId
+        this.animal_type =  
+        this.id = Math.floor(Math.random() * 100)
+        this.question = question
+        this.answer = null
+        this.count = 0
+    }
+
+
+    // category: "Dog",
+    // categoryTag: "#27AE60",
+    // breed_group: "Hound",
+    // primary_breed: "Beagle",
+    // age: "10",
+    // gender: "M",
+    // animal_size: "Small",
+    // primary_color: "Tan",
+    // location: "Downey",
+    // isAvailable: false,
+    // isFavorite: true,
+    // comments: 11,
+    // rating: "4.0",
+    // description: "a"
+}
---
+++ app/services/AdoptionService.js
@@ -0,0 +1,12 @@
+const httpModule = require('tns-core-modules/http')
+
+export default {
+    addTicket(ticket) {
+        return httpModule.request({
+            url: `https://acua-server.herokuapp.com/animals`,
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            content: JSON.stringify(ticket)
+        })
+    }
+}
